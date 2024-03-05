95e0bc4499b41f7660f14f2e66875c954f784de4
Gabriela Silva <gabrielamsilva02@gmail.com>
2024-03-05 00:40:19 -0300

    feature: fix initial migration

--- src/database/migrations/1709599670027-CreateTables.ts
+++ src/database/migrations/1709599670027-CreateTables.ts
@@ -3,23 +3,23 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class CreateTables1709599670027 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`CREATE TABLE "category" (
-        "id" integer,
-        "name" varchar
-      );
-      
-      CREATE TABLE "item" (
-        "id" integer,
-        "category_id" integer NOT NULL,
-        "price" float,
-        "store" varchar,
-        "url" varchar,
-        "image" blob,
-        "priority" varchar,
-        "bought" boolean
-      );
-      
-      ALTER TABLE "item" ADD CONSTRAINT "item_category_id" FOREIGN KEY ("category_id") REFERENCES "category" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+        "id" SERIAL PRIMARY KEY,
+        "name" VARCHAR
+      )
       `);
+    await queryRunner.query(`CREATE TABLE "item" (
+        "id" SERIAL PRIMARY KEY,
+        "category_id" INTEGER NOT NULL,
+        "price" FLOAT NOT NULL,
+        "store" VARCHAR,
+        "url" VARCHAR,
+        "image" BYTEA,
+        "priority" VARCHAR,
+        "bought" BOOLEAN NOT NULL DEFAULT false
+      );`);
+    await queryRunner.query(
+      `ALTER TABLE "item" ADD CONSTRAINT "item_category_id" FOREIGN KEY ("category_id") REFERENCES "category"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
